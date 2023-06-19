ca810ab32def4d61e1877a2f8fee1dcbe707fb85
am999 <am9999072080@gmail.com>
2023-06-19 12:58:25 +0300

    Initial commit

---
+++ src/main/java/ru/skypro/homework_3_3_sql/Homework33SqlApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.homework_3_3_sql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework33SqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework33SqlApplication.class, args);
+    }
+
+}
---
+++ src/main/java/ru/skypro/homework_3_3_sql/exceptions/EntityNotFoundException.java
@@ -0,0 +1,4 @@
+package ru.skypro.homework_3_3_sql.exceptions;
+
+public class EntityNotFoundException {
+}
